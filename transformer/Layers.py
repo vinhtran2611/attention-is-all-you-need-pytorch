e86a00fffc004b5f512a3d049f15b99a1975c894
vinhtran <tranquocvinh2611@gmail.com>
2023-02-07 20:44:40 +0700

    reimplement DecoderLayer

--- transformer/Layers.py
+++ transformer/Layers.py
@@ -27,26 +27,28 @@ class EncoderLayer(nn.Module):
 
 
 class DecoderLayer(nn.Module):
-    ''' Compose with three layers '''
+    """
+    In addition to the two sub-layers in each encoder layer, the decoder inserts a third sub-layer, 
+    which performs multi-head attention over the output of the encoder stack. 
+    Similar to the encoder, we employ residual connections around each of the sub-layers, followed by layer normalization.
+    
+    Decoder is made of self-attn, src-attn, and feed forward (defined below)
+    """
 
-    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
         super(DecoderLayer, self).__init__()
-        self.slf_attn = MultiHeadAttention(
-            n_head, d_model, d_k, d_v, dropout=dropout)
-        self.enc_attn = MultiHeadAttention(
-            n_head, d_model, d_k, d_v, dropout=dropout)
-        self.pos_ffn = PositionwiseFeedForward(
-            d_model, d_inner, dropout=dropout)
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
 
-    def forward(
-            self, dec_input, enc_output,
-            slf_attn_mask=None, dec_enc_attn_mask=None):
-        dec_output, dec_slf_attn = self.slf_attn(
-            dec_input, dec_input, dec_input, mask=slf_attn_mask)
-        dec_output, dec_enc_attn = self.enc_attn(
-            dec_output, enc_output, enc_output, mask=dec_enc_attn_mask)
-        dec_output = self.pos_ffn(dec_output)
-        return dec_output, dec_slf_attn, dec_enc_attn
+    def forward(self, x, memory, src_mask, tgt_mask):
+        "Follow Figure 1 (right) for connections."
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
 
 
 class LayerNorm(nn.Module):
