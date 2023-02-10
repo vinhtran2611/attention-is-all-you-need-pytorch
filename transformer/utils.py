78833d94311e6e86573570cffe23e8a0b96722c8
vinhtran <tranquocvinh2611@gmail.com>
2023-02-10 08:16:00 +0700

    add residual connection module

--- transformer/utils.py
+++ transformer/utils.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch
 import copy
 from transformer.layers.layer_norm import LayerNorm
+from transformer.Models import make_tranformers_model
 import torch.nn.functional as F
 
 def clones(module, N):
@@ -41,3 +42,25 @@ def subsequent_mask(size):
         torch.uint8
     )
     return subsequent_mask == 0
+
+def inference_test():
+    test_model = make_tranformers_model(11, 11, 2)
+    test_model.eval()
+    src = torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
+    src_mask = torch.ones(1, 1, 10)
+
+    memory = test_model.encode(src, src_mask)
+    ys = torch.zeros(1, 1).type_as(src)
+
+    for i in range(9):
+        out = test_model.decode(
+            memory, src_mask, ys, subsequent_mask(ys.size(1)).type_as(src.data)
+        )
+        prob = test_model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data[0]
+        ys = torch.cat(
+            [ys, torch.empty(1, 1).type_as(src.data).fill_(next_word)], dim=1
+        )
+
+    print("Example Untrained Model Prediction:", ys)
