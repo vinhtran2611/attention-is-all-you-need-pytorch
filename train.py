af94689bbcabafdbd8a4d9572ef342d8a8c68b3f
vinhtran <tranquocvinh2611@gmail.com>
2023-02-10 09:40:28 +0700

    implement Training Loop

--- train.py
+++ train.py
@@ -1,19 +1,25 @@
+import time
+
 from transformer.utils import subsequent_mask
+
+# Batches and Masking
+
 
 class Batch:
     """
     Object for holding a batch of data with mask during training.
     """
 
-    def __init__(self, src, tgt=None, pad=2):  
+    def __init__(self, src, tgt=None, pad=2):
         """
         Args:
             src: (batch_size, src_vocab)
             tgt: (batch_size, tgt_vocab)
             2 = <blank>
         """
-        self.src = src 
-        self.src_mask = (src != pad).unsqueeze(-2) # (batch_size, 1, src_vocab)
+        self.src = src
+        # (batch_size, 1, src_vocab)
+        self.src_mask = (src != pad).unsqueeze(-2)
 
         if tgt is not None:
             self.tgt = tgt[:, :-1]
@@ -29,3 +35,70 @@ class Batch:
             tgt_mask.data
         )
         return tgt_mask
+
+
+# Training Loop
+class TrainState:
+    """Track number of steps, examples, and tokens processed"""
+
+    step: int = 0  # Steps in the current epoch
+    accum_step: int = 0  # Number of gradient accumulation steps
+    samples: int = 0  # total of examples used
+    tokens: int = 0  # total of tokens processed
+
+
+def run_epoch(
+    data_iter,
+    model,
+    loss_compute,
+    optimizer,
+    scheduler,
+    mode="train",
+    accum_iter=1,
+    train_state=TrainState(),
+):
+    """Train a single epoch"""
+
+    start = time.time()
+    total_tokens = 0
+    total_loss = 0
+    tokens = 0
+    n_accum = 0
+
+    for i, batch in enumerate(data_iter):
+        out = model.forward(
+            batch.src, batch.tgt, batch.src_mask, batch.tgt_mask
+        )
+        # loss_node = loss_node / accum_iter
+        loss, loss_node = loss_compute(out, batch.tgt_y, batch.ntokens)
+
+        if mode == "train" or mode == "train+log":
+            loss_node.backward()
+            train_state.step += 1
+            train_state.samples += batch.src.shape[0]
+            train_state.tokens += batch.ntokens
+            if i % accum_iter == 0:
+                optimizer.step()
+                optimizer.zero_grad(set_to_none=True)
+                n_accum += 1
+                train_state.accum_step += 1
+            scheduler.step()
+
+        total_loss += loss
+        total_tokens += batch.ntokens
+        tokens += batch.ntokens
+        if i % 40 == 1 and (mode == "train" or mode == "train+log"):
+            lr = optimizer.param_groups[0]["lr"]
+            elapsed = time.time() - start
+            print(
+                (
+                    "Epoch Step: %6d | Accumulation Step: %3d | Loss: %6.2f "
+                    + "| Tokens / Sec: %7.1f | Learning Rate: %6.1e"
+                )
+                % (i, n_accum, loss / batch.ntokens, tokens / elapsed, lr)
+            )
+            start = time.time()
+            tokens = 0
+        del loss
+        del loss_node
+    return total_loss / total_tokens, train_state
